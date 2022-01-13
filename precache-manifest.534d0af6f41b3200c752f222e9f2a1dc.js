8dd655d844e3eced64ac9b4109b74259a1cce5d9
Prodev <goforprodev@gmail.com>
2022-01-13 16:28:37 +0100

    Updates

---
+++ precache-manifest.534d0af6f41b3200c752f222e9f2a1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdaf512797e2ba88bd9273680cf2c94",
+    "url": "/monsterz-rolodex/index.html"
+  },
+  {
+    "revision": "4d750ef11c527c52181e",
+    "url": "/monsterz-rolodex/static/css/main.980c1ead.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/monsterz-rolodex/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "dfa8c23334d711f8ea6c",
+    "url": "/monsterz-rolodex/static/js/3.fb07916a.chunk.js"
+  },
+  {
+    "revision": "4d750ef11c527c52181e",
+    "url": "/monsterz-rolodex/static/js/main.8bd1cae3.chunk.js"
+  },
+  {
+    "revision": "c84b64d54dad37fa933d",
+    "url": "/monsterz-rolodex/static/js/runtime~main.b0196c3e.js"
+  }
+]);
